a16f492a71a6920da6744e7f57cb4c695c15bd0c
codemetin <textbynext@gmail.com>
2024-10-16 21:35:45 +0300

    donguler

---
+++ gun23/donguler.js
@@ -0,0 +1,9 @@
+let n = 1; // ilk değer atama (initialization)
+
+while (n < 6) {  // koşul (condition)
+    console.log(n);
+    n++; // arttırma / azaltma (increment - decrement)
+}
+
+console.log("bye bye");
+console.log(n);
